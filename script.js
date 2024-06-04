4d7d1fdcb7375eda6eb50fbc5d1729b3d4376706
Vanessa Camargo <158234704+CamargoVanessa@users.noreply.github.com>
2024-06-03 21:02:22 -0300

    Correção de arquivos

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-let textInput = document.querySelector("#campo__input__texto");
-let outInput = document.querySelector("#campo__resposta");
+let inputTexto = document.getElementById("campo__input__texto");
+let campoResposta = document.getElementById("campo__resposta");
 
 function criptografar() {
-    let texto = textInput.value;
+  let texto = inputTexto.value;
 
-    let resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g,"ufat");
+  let resultCripto = texto
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat");
 
-    document.getElementById('campo__resposta').innerHTML = '<textarea readonly id="campo__output__texto" rows="8" cols="35">' + resultCripto + '</textarea>' + '<button class="botao__copiar" id="copiar" onclick="copiar()">Copiar</button>'
+  campoResposta.innerHTML = '<textarea id="campo__output__texto" rows="15" cols="35">' + resultCripto + '</textarea><button class="botao__copiar" id="copiar" onclick="copiar()">Copiar</button>';
 }
 
 function descriptografar() {
-    let texto = textInput.value;
+  let texto = inputTexto.value;
 
-    let resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g,"u");
+  let resultDescripto = texto
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
 
-    document.getElementById('campo__resposta').innerHTML = '<textarea readonly id="campo__output__texto" rows="8" cols="35">' + resultDescripto + '</textarea>' + '<button class="botao__copiar" id="copiar" onclick="copiar()">Copiar</button>'
+  campoResposta.innerHTML = '<textarea readonly id="campo__output__texto" rows="15" cols="35">'  + resultDescripto + '</textarea><button class="botao__copiar" id="copiar" onclick="copiar()">Copiar</button>';
 }
 
 function copiar() {
-    let textoCop = document.getElementById("campo__output__texto").value;
-    navigator.clipboard.writeText(textoCop);
-    alert("Texto Copiado!");
+  let textoCop = document.getElementById("campo__output__texto").value;
+  navigator.clipboard.writeText(textoCop);
+  alert("Texto Copiado!");
 }
